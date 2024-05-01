e7314beca5cc56967972b71e056172870287585b
kdrbipul <kdrbipul@gmail.com>
2024-05-01 11:29:52 +0600

    initial setup

---
+++ lib/app.dart
@@ -0,0 +1,13 @@
+import 'package:ecommerce_project/presentation/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+class CraftyBay extends StatelessWidget {
+  const CraftyBay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SplashScreen(),
+    );
+  }
+}
