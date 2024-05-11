ea2d7fea69f98554086ecac7e8c7acf9ab545be0
kdrbipul <kdrbipul@gmail.com>
2024-05-11 12:20:54 +0600

    carousel part add in home screen

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:ecommerce_project/presentation/utils/app_color.dart';
 import 'package:ecommerce_project/presentation/utils/assets_path.dart';
 import 'package:ecommerce_project/widgets/app_bar_icon_button.dart';
 import 'package:flutter/material.dart';
@@ -21,33 +23,67 @@ class _HomeScreenState extends State<HomeScreen> {
         padding: const EdgeInsets.all(16),
         child: Column(
           children: [
-            TextField(
-              controller: _searchTEController,
-              keyboardType: TextInputType.text,
-              decoration: InputDecoration(
-                hintText: 'Search',
-                fillColor: Colors.grey.shade200,
-                filled: true,
-                prefixIcon: const Icon(
-                  Icons.search,
-                  color: Colors.grey,
-                ),
-                enabledBorder:  OutlineInputBorder(
-                  borderSide: BorderSide.none,
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                focusedBorder:  OutlineInputBorder(
-                  borderSide: BorderSide.none,
-                  borderRadius: BorderRadius.circular(8),
-                ),
+            _buildSearchTextField(),
+            const SizedBox(height: 16,),
+            CarouselSlider(
+              options: CarouselOptions(
+                height: 200.0,
+                viewportFraction: 1,
               ),
-            ),
+              items: [1, 2, 3, 4, 5].map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                      alignment: Alignment.center,
+                      width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.symmetric(
+                        horizontal: 5.0,
+                      ),
+                      decoration: BoxDecoration(
+                        color: AppColors.primaryColor,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Text(
+                        'text $i',
+                        style: const TextStyle(
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    );
+                  },
+                );
+              }).toList(),
+            )
           ],
         ),
       )),
     );
   }
 
+
+  Widget _buildSearchTextField() {
+    return TextField(
+      controller: _searchTEController,
+      keyboardType: TextInputType.text,
+      decoration: InputDecoration(
+        hintText: 'Search',
+        fillColor: Colors.grey.shade200,
+        filled: true,
+        prefixIcon: const Icon(
+          Icons.search,
+          // color: Colors.grey,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+    );
+  }
   AppBar _buildAppBar() {
     return AppBar(
       title: SvgPicture.asset(AssetsPath.navLogoSvg),
