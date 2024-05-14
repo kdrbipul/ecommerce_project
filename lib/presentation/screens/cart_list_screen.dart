08b4f23a97ad420d452e478d47996ffca8ee4c65
kdrbipul <kdrbipul@gmail.com>
2024-05-14 17:06:40 +0600

    cart screen complete

--- lib/presentation/screens/cart_list_screen.dart
+++ lib/presentation/screens/cart_list_screen.dart
@@ -1,5 +1,12 @@
+import 'dart:ui';
+
 import 'package:ecommerce_project/presentation/state_holders/main_bottom_nav_bar_controller.dart';
+import 'package:ecommerce_project/presentation/utils/app_color.dart';
+import 'package:ecommerce_project/widgets/cart_product.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
 class CartListScreen extends StatefulWidget {
@@ -10,6 +17,7 @@ class CartListScreen extends StatefulWidget {
 }
 
 class _CartListScreenState extends State<CartListScreen> {
+
   @override
   Widget build(BuildContext context) {
     return  PopScope(
@@ -27,7 +35,66 @@ class _CartListScreenState extends State<CartListScreen> {
             icon: const Icon(Icons.arrow_back_ios),
           ),
         ),
+        body:    Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return const CartProduct();
+                },
+              ),
+            ),
+            _buildCheckOutWidget()
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _buildCheckOutWidget() {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+          color: AppColors.primaryColor.withOpacity(0.1),
+          borderRadius: const BorderRadius.only(
+            topRight: Radius.circular(16),
+            topLeft: Radius.circular(16),
+          )),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          _buildTotalPriceWidget(),
+          SizedBox(
+              width: 100,
+              child: ElevatedButton(
+                  onPressed: () {}, child: const Text('Checkout')))
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTotalPriceWidget() {
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Total Price',
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Colors.grey,
+          ),
+        ),
+        Text(
+          '\$400',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w600,
+            color: AppColors.primaryColor,
+          ),
+        ),
+      ],
     );
   }
 }
---
+++ lib/widgets/cart_product.dart
@@ -0,0 +1,145 @@
+import 'package:ecommerce_project/presentation/utils/app_color.dart';
+import 'package:ecommerce_project/presentation/utils/assets_path.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:item_count_number_button/item_count_number_button.dart';
+
+class CartProduct extends StatefulWidget {
+  const CartProduct({super.key});
+
+  @override
+  State<CartProduct> createState() => _CartProductState();
+}
+
+class _CartProductState extends State<CartProduct> {
+  int _currentValue = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4,
+      margin: const EdgeInsets.symmetric(
+        horizontal: 16,
+        vertical: 4,
+      ),
+      surfaceTintColor: Colors.white,
+      child: Row(
+        children: [
+          _buildProductImage(),
+          Expanded(
+            child: _buildProductDetails(),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProductDetails() {
+    return Column(
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    _buildProductName(),
+                    _buildProductCategory(),
+                  ],
+                ),
+              ),
+            ),
+            _buildProductIconButton(),
+          ],
+        ),
+        _buildProductCartBottomContent(),
+      ],
+    );
+  }
+
+  Widget _buildProductCartBottomContent() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Text(
+            '\$400',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: AppColors.primaryColor,
+            ),
+          ),
+          _buildItemItemCount(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProductIconButton() {
+    return IconButton(
+      onPressed: () {},
+      icon: const Icon(
+        Icons.delete_outline,
+      ),
+    );
+  }
+
+  Widget _buildProductCategory() {
+    return const Wrap(
+      spacing: 16,
+      children: [
+        Text(
+          'Color: Red',
+          style: TextStyle(
+              fontSize: 16, fontWeight: FontWeight.w400, color: Colors.grey),
+        ),
+        Text(
+          'Size: xl',
+          style: TextStyle(
+              fontSize: 16, fontWeight: FontWeight.w400, color: Colors.grey),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildProductName() {
+    return const Text(
+      'Nike Shoe 234555',
+      maxLines: 1,
+      style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w500,
+          color: Colors.black87,
+          overflow: TextOverflow.ellipsis),
+    );
+  }
+
+  Widget _buildItemItemCount() {
+    return ItemCount(
+      initialValue: _currentValue,
+      minValue: 1,
+      maxValue: 20,
+      decimalPlaces: 0,
+      color: AppColors.primaryColor,
+      onChanged: (value) {
+        _currentValue = value as int;
+        setState(() {});
+      },
+    );
+  }
+
+  Widget _buildProductImage() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SvgPicture.asset(
+        AssetsPath.productShoeSvg,
+        width: 120,
+      ),
+    );
+  }
+}
