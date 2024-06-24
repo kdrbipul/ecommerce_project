3735a7f4dbc85934958e536b7e2efb300c8d2f7e
kdrbipul <kdrbipul@gmail.com>
2024-06-24 12:54:08 +0600

    cart list screen correction

--- lib/presentation/screens/cart_list_screen.dart
+++ lib/presentation/screens/cart_list_screen.dart
@@ -9,6 +9,7 @@ import 'package:get/get.dart';
 class CartListScreen extends StatefulWidget {
   const CartListScreen({super.key});
 
+
   @override
   State<CartListScreen> createState() => _CartListScreenState();
 }
